5836be49a0c157ab341e24c0a658411155c1790d
galpHub <galpHub@users.noreply.github.com>
2015-09-27 14:50:51 -0600

    Fixed a bug with activation_function and implemented tanh nn

--- nn.cpp
+++ nn.cpp
@@ -1,17 +1,18 @@
 // Implementation of feedforward neural networks with backpropagation.
 #include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
 #define RECT 0
 #define TANH 1
-
 
 
 typedef double (*activation_function)(double*,double*,int); // typedef double (*)() activation_function;
 
 
 class neuron{
-	int indegree;
+	int indegree; // the number of neurons from the lower layer connected to this neuron.
 	int* neuron_idcs; // Indices of neurons in the previous (lower) layer this neuron requires access to.
-	double* weights; // Weights for the computation of the neuron signal.
+	double* weights; // Weights for the computation of the neuron signal - including the bias as the last entry.
 	activation_function activation; // Activation function for the neuron.
 	char type[4]; // Short string descriptor of the activation function.
 	double value = 0; // Starting neuron signal.
@@ -31,6 +32,15 @@ void neuron::fire(double* input){
 	
 }
 
+void neuron::def_activation(){
+	if (type == "rect"){
+		activation = activation_type(RECT);
+	}
+	else if (type == "tanh"){
+		activation = activation_type(TANH);
+	}
+}
+
 
 class layer{
 };
@@ -39,11 +49,12 @@ class n_network{
 };
 
 
-double rect(double* weights, double* input, int length){
+double n_rect(double* weights, double* input, int length){
 	double sum = 0;
 	for (int i = 0; i < length; i++){
 		sum += weights[i] * input[i];
 	}
+	sum += weights[length];
 
 	if (sum >1){
 		return 1.0;
@@ -56,13 +67,23 @@ double rect(double* weights, double* input, int length){
 	}
 }
 
+double n_tanh(double* weights, double* input, int length){
+	double sum = 0;
+	for (int i = 0; i < length; i++){
+		sum += weights[i] * input[i];
+	}
+	sum += weights[length];
+
+	return 0.5*tanh(sum)+1;
+}
+
 
 activation_function activation_type(int type){
 	switch (type){
 		case RECT:
-			return (activation_function) rect;
-		//case TANH:
-		//	return (activation_function) tanh;
+			return (activation_function) n_rect;
+		case TANH:
+			return (activation_function) n_tanh;
 		default:
 			throw -1;
 	}
@@ -75,14 +96,14 @@ activation_function activation_type(int type){
 
 
 int main(int argc, char** argv){
-	double test_weights[2] = {1.0,2.0};
+	double test_weights[3] = {1.0,2.0,0.0};
 	double test_input1[2] = { -2.0/3.0, 2.0/3.0 };
 	double test_input2[2] = {1.0,0.0};
 	printf("This is a test: ");
 	getchar();
-	printf("%f, %f\n", rect(test_weights, test_input1, 2), rect(test_weights, test_input2, 2));
+	printf("%f, %f\n", n_rect(test_weights, test_input1, 2), n_rect(test_weights, test_input2, 2));
 	getchar();
-	activation_function thefunction = activation_type(0);
+	activation_function thefunction = activation_type(RECT);
 	printf("This is the doom: %f\n", thefunction(test_weights, test_input1, 2) );
 	getchar();
 
