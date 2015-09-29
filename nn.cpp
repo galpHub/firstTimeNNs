5751feb7ddfbdb91477dbdab960af2f7dd386c12
galpHub <galpHub@users.noreply.github.com>
2015-09-29 11:15:47 -0600

    Added more comments to clarify function usage. Refactored activation to activation_f in the neuron class.

--- nn.cpp
+++ nn.cpp
@@ -20,13 +20,16 @@ double random_gen(){
 // Ease of referencing a function pointer for neurons to get their activation functions from the same place.
 typedef double (*activation_function)(double*,double*,int);
 
-
+// The neuron class. Implements a neuron by specifying the indices of the neurons in a previous layer from
+// which to take its inputs, what the weights corresponding to those inputs (with an additional bias term)
+// are, the type of neuron and the corresponding activation function in case type is specified. The input
+// layer doesn't have an activation function.
 class neuron{
 	public:
 		int indegree = 0; // the number of neurons from the lower layer connected to this neuron.
 		int* neuron_idcs = NULL; // Indices of neurons in the previous (lower) layer this neuron requires access to.
 		double* weights = NULL; // Weights for the computation of the neuron signal - including the bias as the last entry.
-		activation_function activation; // Activation function for the neuron.
+		activation_function activation_f; // Activation function for the neuron.
 		std::string type; // Short string descriptor of the activation function.
 		double value = 0; // Starting neuron signal.
 
@@ -56,6 +59,8 @@ neuron::neuron(int* input_neurons, int num_inputs, std::string act_type){
 	}
 }
 
+
+// Implementation of activation functions for neurons.
 double n_rect(double* weights, double* input, int length){
 	double sum = 0;
 	for (int i = 0; i < length; i++){
@@ -84,7 +89,7 @@ double n_tanh(double* weights, double* input, int length){
 	return 0.5*tanh(sum) + 1;
 }
 
-
+// A get-er function for activation functions 
 activation_function activation_type(int type){
 	switch (type){
 	case RECT:
@@ -96,10 +101,10 @@ activation_function activation_type(int type){
 	}
 }
 
-
+// Updates the value of the "values" variable in the neuron by evaluating the activation function.
 void neuron::fire(double* input){
-	if (!activation){
-		value = activation(weights, input, indegree);
+	if (!activation_f){
+		value = activation_f(weights, input, indegree);
 	}
 	else{
 		value = 0;
@@ -107,12 +112,14 @@ void neuron::fire(double* input){
 	
 }
 
+// Initializes the activation function of a neuron based on the value of "type", either rectilinear (RECT) or hyberbolic tangent
+// (TANH) currently implemented.
 void neuron::def_activation(){
 	if (type == "rect"){
-		activation = activation_type(RECT);
+		activation_f = activation_type(RECT);
 	}
 	else if (type == "tanh"){
-		activation = activation_type(TANH);
+		activation_f = activation_type(TANH);
 	}
 }
 
