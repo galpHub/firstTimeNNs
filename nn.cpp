1ab1cdf2a0785f3693ae5be409fbaf8a9a26a6f6
galpHub <galpHub@users.noreply.github.com>
2015-09-29 22:56:24 -0600

    Found some bugs with the def_activation function and the neuron constructor.

--- nn.cpp
+++ nn.cpp
@@ -18,7 +18,7 @@ double random_gen(){
 }
 
 // Ease of referencing a function pointer for neurons to get their activation functions from the same place.
-typedef double (*activation_function)(double*,double*,int);
+typedef double (*activation_function)(std::vector<double>, double*, int);
 
 // The neuron class. Implements a neuron by specifying the indices of the neurons in a previous layer from
 // which to take its inputs, what the weights corresponding to those inputs (with an additional bias term)
@@ -27,8 +27,8 @@ typedef double (*activation_function)(double*,double*,int);
 class neuron{
 	public:
 		int indegree = 0; // the number of neurons from the lower layer connected to this neuron.
-		int* neuron_idcs = NULL; // Indices of neurons in the previous (lower) layer this neuron requires access to.
-		double* weights = NULL; // Weights for the computation of the neuron signal - including the bias as the last entry.
+		std::vector<int> neuron_idcs; // Indices of neurons in the previous (lower) layer this neuron requires access to.
+		std::vector<double> weights; // Weights for the computation of the neuron signal - including the bias as the last entry.
 		activation_function activation_f; // Activation function for the neuron.
 		std::string type; // Short string descriptor of the activation function.
 		double value = 0; // Starting neuron signal.
@@ -49,17 +49,19 @@ class neuron{
 neuron::neuron(int* input_neurons, int num_inputs, std::string act_type){
 	indegree = num_inputs;
 	type = act_type;
-	std::vector<double> weights(num_inputs,0.0);
-	std::vector<int> neuron_idcs(num_inputs, 0);
+
+	weights.reserve(num_inputs + 1);
+	neuron_idcs.reserve(num_inputs);
+
 	neuron::def_activation();
 	for (int i = 0; i < num_inputs + 1; i++){
-		weights[i] = random_gen();
+		weights.push_back(random_gen());
 	}
 }
 
 
 // Implementation of activation functions for neurons.
-double n_rect(double* weights, double* input, int length){
+double n_rect(std::vector<double> weights, double* input, int length){
 	double sum = 0;
 	for (int i = 0; i < length; i++){
 		sum += weights[i] * input[i];
@@ -77,14 +79,14 @@ double n_rect(double* weights, double* input, int length){
 	}
 }
 
-double n_tanh(double* weights, double* input, int length){
+double n_tanh(std::vector<double> weights, double* input, int length){
 	double sum = 0;
 	for (int i = 0; i < length; i++){
 		sum += weights[i] * input[i];
 	}
 	sum += weights[length];
 
-	return 0.5*tanh(sum) + 1;
+	return 0.5*(tanh(sum) + 1);
 }
 
 // A get-er function for activation functions 
@@ -101,7 +103,7 @@ activation_function activation_type(int type){
 
 // Updates the value of the "values" variable in the neuron by evaluating the activation function.
 void neuron::fire(double* input){
-	if (!activation_f){
+	if (activation_f!=NULL){
 		value = activation_f(weights, input, indegree);
 	}
 	else{
@@ -140,19 +142,6 @@ class n_network{
 
 
 int main(int argc, char** argv){
-
-	double test_weights[3] = {1.0,2.0,0.0};
-	double test_input1[2] = { -2.0/3.0, 2.0/3.0 };
-	double test_input2[2] = {1.0,0.0};
-	printf("This is a test of the activation functions' output: \n");
-	printf("Direct output of the rect functions in two sample cases: \n");
-	printf("%f, %f\n\n", n_rect(test_weights, test_input1, 2), n_rect(test_weights, test_input2, 2));
-	getchar();
-	activation_function function1 = activation_type(RECT);
-	activation_function function2 = activation_type(RECT);
-	printf("This is the outputs of the rect activation function called via activation_type: %f, %f\n",
-		function1(test_weights, test_input1, 2), function2(test_weights, test_input2, 2));
-	getchar();
-
+	
 	return 0;
 }
